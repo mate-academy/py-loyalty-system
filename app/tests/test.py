5a7ec5a9d376a5f5e56892b06e760451e4a90e37
Nataliia Zakharchuk <62181026+Nattalli@users.noreply.github.com>
2022-08-11 15:26:49 +0300

    Update test.py

--- app/tests/test.py
+++ app/tests/test.py
@@ -11,11 +11,9 @@ from main import (
 @pytest.mark.django_db
 def test_all_loyalty_program_names(django_db_setup):
     result = all_loyalty_program_names()
-    assert list(result) == [
-        ("Base level", 5), 
-        ("Middle level", 10), 
-        ("Gold level", 20)
-    ]
+    assert list(result) == [("Base level", 5),
+                            ("Middle level", 10),
+                            ("Gold level", 20)]
 
 
 @pytest.mark.django_db
