0be64cc27127095341bfa7ecc52608525f19bc7c
dstarkaiev <dstarkaiev@gmail.com>
2022-08-11 14:10:12 +0300

    Revert "solution"
    This reverts commit 026f0f7b3b6502a46f9824e60c079c1335c8ff2a.

--- main.py
+++ main.py
@@ -1,37 +1,23 @@
-from django.db.models import Q, F
-
-import init_django_orm  # noqa: F401
+import init_django_orm
 
 from app.models import Customer, LoyaltyProgram, LoyaltyProgramParticipant
 
 
 def all_loyalty_program_names():
-    return LoyaltyProgram.objects.all().values_list("name", "bonus_percentage")
+    pass
 
 
 def not_active_customers():
-    return LoyaltyProgramParticipant.objects.filter(
-        last_activity__lt="2022-01-01",
-        last_activity__gt="2021-01-01"
-    ).values("customer__first_name")
+    pass
 
 
 def most_active_customers():
-    return Customer.objects.all().order_by(
-        "-loyaltyprogramparticipant__sum_of_spent_money"
-    ).values_list(
-        "first_name", "last_name",
-        "loyaltyprogramparticipant__sum_of_spent_money"
-    )[:5]
+    pass
 
 
 def clients_with_i_and_k():
-    return Customer.objects.filter(
-        Q(first_name__startswith="I") | Q(last_name__contains="o")
-    )
+    pass
 
 
 def bonuses_less_then_spent_money():
-    return LoyaltyProgramParticipant.objects.filter(
-        active_bonuses__lt=F("sum_of_spent_money")
-    ).values("customer__phone_number")
+    pass
