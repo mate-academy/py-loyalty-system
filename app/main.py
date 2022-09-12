e12451ba0ca7233327865c597b8997fe8a7a66f4
Diliaver Yusupov <diliaver795@gmail.com>
2022-09-12 14:32:47 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -11,34 +11,28 @@ def all_loyalty_program_names():
 
 
 def not_active_customers():
-    not_active_customers = \
-        LoyaltyProgramParticipant.objects.filter(
-            last_activity__gt="2021-01-01",
-            last_activity__lt="2022-01-01").values("customer__first_name")
 
-    return not_active_customers
+    return LoyaltyProgramParticipant.objects.filter(
+        last_activity__gt="2021-01-01",
+        last_activity__lt="2022-01-01").values("customer__first_name")
 
 
 def most_active_customers():
-    most_active_customers = LoyaltyProgramParticipant.objects.all()
-    most_active_customers = LoyaltyProgramParticipant.objects.order_by(
+
+    return LoyaltyProgramParticipant.objects.order_by(
         '-sum_of_spent_money')[0:5].values_list("customer__first_name",
                                                 "customer__last_name",
                                                 "sum_of_spent_money")
 
-    return most_active_customers
-
 
 def clients_with_i_and_k():
 
-    clients_with_i_and_k = Customer.objects.filter(
+    return Customer.objects.filter(
         Q(first_name__startswith="I") | Q(last_name__contains="o"))
-
-    return clients_with_i_and_k
 
 
 def bonuses_less_then_spent_money():
-    bonuses_less_then_spent_money = LoyaltyProgramParticipant.objects.filter(
+
+    return LoyaltyProgramParticipant.objects.filter(
         active_bonuses__lt=F("sum_of_spent_money")).values(
         "customer__phone_number")
-    return bonuses_less_then_spent_money
