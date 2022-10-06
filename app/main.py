22174d2e4bae46ac669dbd19b91a7974b75c9286
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-10-06 12:58:12 +0300

    'py_loyalty_system_Solution_4'

--- app/main.py
+++ app/main.py
@@ -1,21 +1,44 @@
+import datetime
+
 import init_django_orm  # noqa: F401
+
+from django.db.models import F
+
+from db.models import LoyaltyProgram, LoyaltyProgramParticipant, Customer
 
 
 def all_loyalty_program_names():
-    pass
+    return LoyaltyProgram.objects.all().values_list("name", "bonus_percentage")
 
 
 def not_active_customers():
-    pass
+    start = datetime.date(2021, 1, 1)
+    end = datetime.date(2022, 1, 1)
+    return LoyaltyProgramParticipant.objects.filter(
+        last_activity__date__gte=start,
+        last_activity__date__lte=end).values(
+        "customer")
+    # return LoyaltyProgramParticipant.objects.all().values_list(
+    #     "customer__first_name"
+    # ).filter(
+    #     Q(last_activity__gte="2021-01-01") & Q(last_activity__lt="2022-01-01")
+    # ).values("customer__first_name")
 
 
 def most_active_customers():
-    pass
+    return LoyaltyProgramParticipant.objects.all().values_list(
+        "customer__first_name",
+        "customer__last_name",
+        "sum_of_spent_money"
+    ).order_by("-sum_of_spent_money")[:5]
 
 
 def clients_with_i_and_o():
-    pass
+    return Customer.objects.filter(
+        first_name__startswith="I", last_name__contains="o")
 
 
 def bonuses_less_then_spent_money():
-    pass
+    return LoyaltyProgramParticipant.objects.filter(
+        active_bonuses__lt=F("sum_of_spent_money")
+    ).values("customer__phone_number")
