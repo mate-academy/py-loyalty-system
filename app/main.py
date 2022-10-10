95d5eecbe9bdd310c3905728d15a3333cbc59cbc
danylott <d.tuytuyshkin@gmail.com>
2022-10-10 15:20:20 +0300

    Added flake8 plugins

--- app/main.py
+++ app/main.py
@@ -1,21 +1,23 @@
 import init_django_orm  # noqa: F401
 
+from django.db.models import QuerySet
 
-def all_loyalty_program_names():
+
+def all_loyalty_program_names() -> QuerySet:
     pass
 
 
-def not_active_customers():
+def not_active_customers() -> QuerySet:
     pass
 
 
-def most_active_customers():
+def most_active_customers() -> QuerySet:
     pass
 
 
-def clients_with_i_and_o():
+def clients_with_i_and_o() -> QuerySet:
     pass
 
 
-def bonuses_less_then_spent_money():
+def bonuses_less_then_spent_money() -> QuerySet:
     pass
