2436d7bbde6d3a4b02e12763fb6448f99c296b14
mltdll <ilya.vyplavin@gmail.com>
2022-10-16 23:47:42 +0300

    Remove unnecessary `all()` calls

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from db.models import Customer, LoyaltyProgram, LoyaltyProgramParticipant
 
 
 def all_loyalty_program_names() -> QuerySet:
-    return LoyaltyProgram.objects.all().values_list("name", "bonus_percentage")
+    return LoyaltyProgram.objects.values_list("name", "bonus_percentage")
 
 
 def not_active_customers() -> QuerySet:
@@ -17,7 +17,7 @@ def not_active_customers() -> QuerySet:
 
 def most_active_customers() -> QuerySet:
     return (
-        LoyaltyProgramParticipant.objects.all()
+        LoyaltyProgramParticipant.objects
         .order_by("-sum_of_spent_money")
         .values_list(
             "customer__first_name",
