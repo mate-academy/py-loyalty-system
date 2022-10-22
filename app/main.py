7f8da585c658ec6f43a6cc50f8023f42235a4a4e
Egor Voloshyn <103663723+AriohBelskij@users.noreply.github.com>
2022-10-22 12:19:38 +0300

    Make simples and remove /

--- app/main.py
+++ app/main.py
@@ -4,38 +4,34 @@ from django.db.models import F, Q, QuerySet
 
 
 def all_loyalty_program_names() -> QuerySet:
-    queryset = LoyaltyProgram.objects.filter().values_list("name",
-                                                           "bonus_percentage")
+    queryset = LoyaltyProgram.objects.values_list("name", "bonus_percentage")
     return queryset
 
 
 def not_active_customers() -> QuerySet:
     queryset = LoyaltyProgramParticipant.objects.filter(
-        last_activity__lt="2022-01-01",
-        last_activity__gt="2021-01-01"
+        last_activity__year="2021"
     ).values("customer__first_name")
     return queryset
 
 
 def most_active_customers() -> QuerySet:
-    queryset = LoyaltyProgramParticipant.objects.\
-        filter().values_list("customer__first_name",
-                             "customer__last_name",
-                             "sum_of_spent_money"
-                             ).order_by("-sum_of_spent_money")[:5]
+    queryset = LoyaltyProgramParticipant.objects.values_list(
+        "customer__first_name", "customer__last_name", "sum_of_spent_money"
+    ).order_by("-sum_of_spent_money")[:5]
     return queryset
 
 
 def clients_with_i_and_o() -> QuerySet:
-    queryset = LoyaltyProgramParticipant. \
-        objects.filter(
-            Q(customer__first_name__startswith="I") | Q
-            (customer__last_name__contains="o"))
+    queryset = LoyaltyProgramParticipant.objects.filter(
+        Q(customer__first_name__startswith="I")
+        | Q(customer__last_name__contains="o")
+    )
     return queryset
 
 
 def bonuses_less_then_spent_money() -> QuerySet:
-    queryset = LoyaltyProgramParticipant.objects. \
-        filter(active_bonuses__lt=F("sum_of_spent_money")). \
-        values("customer__phone_number")
+    queryset = LoyaltyProgramParticipant.objects.filter(
+        active_bonuses__lt=F("sum_of_spent_money")
+    ).values("customer__phone_number")
     return queryset
--- db/migrations/0001_initial.py
+++ db/migrations/0001_initial.py
@@ -8,36 +8,74 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Customer',
+            name="Customer",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('first_name', models.CharField(max_length=64)),
-                ('last_name', models.CharField(max_length=64)),
-                ('phone_number', models.CharField(max_length=20)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("first_name", models.CharField(max_length=64)),
+                ("last_name", models.CharField(max_length=64)),
+                ("phone_number", models.CharField(max_length=20)),
             ],
         ),
         migrations.CreateModel(
-            name='LoyaltyProgram',
+            name="LoyaltyProgram",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=64)),
-                ('bonus_percentage', models.IntegerField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=64)),
+                ("bonus_percentage", models.IntegerField()),
             ],
         ),
         migrations.CreateModel(
-            name='LoyaltyProgramParticipant',
+            name="LoyaltyProgramParticipant",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('last_activity', models.DateField(auto_now=True)),
-                ('active_bonuses', models.IntegerField(blank=True, default=0, null=True)),
-                ('sum_of_spent_money', models.IntegerField(default=0)),
-                ('customer', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='db.customer')),
-                ('loyalty_program', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='db.loyaltyprogram')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("last_activity", models.DateField(auto_now=True)),
+                (
+                    "active_bonuses",
+                    models.IntegerField(blank=True, default=0, null=True),
+                ),
+                ("sum_of_spent_money", models.IntegerField(default=0)),
+                (
+                    "customer",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="db.customer",
+                    ),
+                ),
+                (
+                    "loyalty_program",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        to="db.loyaltyprogram",
+                    ),
+                ),
             ],
         ),
     ]
