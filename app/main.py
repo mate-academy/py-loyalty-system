c93fd5015537af567337d982966418a7e9d17f0f
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-10-24 11:10:14 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -16,9 +16,7 @@ def not_active_customers() -> QuerySet:
 
 
 def most_active_customers() -> QuerySet:
-    return LoyaltyProgramParticipant.objects.all().order_by(
-        "sum_of_spent_money"
-    ).values_list(
+    return LoyaltyProgramParticipant.objects.all().values_list(
         "customer__first_name",
         "customer__last_name",
         "sum_of_spent_money"
