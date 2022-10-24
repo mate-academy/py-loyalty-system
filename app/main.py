e78e7e21d5c1b147e7c46d745f207fb07bb44308
Kateryna Kilyk <katekilik@gmail.com>
2022-10-24 11:12:30 +0300

    Redundant variable deleted

--- app/main.py
+++ app/main.py
@@ -6,39 +6,34 @@ from db.models import Customer, LoyaltyProgram, LoyaltyProgramParticipant
 
 
 def all_loyalty_program_names() -> QuerySet:
-    query = LoyaltyProgram.objects.values_list(
+    return LoyaltyProgram.objects.values_list(
         "name", "bonus_percentage"
     )
-    return query
 
 
 def not_active_customers() -> QuerySet:
-    query = LoyaltyProgramParticipant.objects.filter(
+    return LoyaltyProgramParticipant.objects.filter(
         last_activity__year=2021
     ).values("customer__first_name")
-    return query
 
 
 def most_active_customers() -> QuerySet:
-    query = LoyaltyProgramParticipant.objects.order_by(
+    return LoyaltyProgramParticipant.objects.order_by(
         "-sum_of_spent_money"
     ).values_list(
         "customer__first_name",
         "customer__last_name",
         "sum_of_spent_money"
     )[:5]
-    return query
 
 
 def clients_with_i_and_o() -> QuerySet:
-    query = Customer.objects.filter(
+    return Customer.objects.filter(
         Q(first_name__startswith="I") | Q(last_name__contains="o")
     )
-    return query
 
 
 def bonuses_less_then_spent_money() -> QuerySet:
-    query = LoyaltyProgramParticipant.objects.filter(
+    return LoyaltyProgramParticipant.objects.filter(
         active_bonuses__lt=F("sum_of_spent_money")
     ).values("customer__phone_number")
-    return query
