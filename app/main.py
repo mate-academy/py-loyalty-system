0b805c9661f593e786d919aae67c95ca9bea9682
Nikyxa <kolisova.veronika81@gmail.com>
2022-10-25 20:34:46 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -6,20 +6,33 @@ from db.models import Customer, LoyaltyProgram, LoyaltyProgramParticipant
 
 
 def all_loyalty_program_names() -> QuerySet:
-    return LoyaltyProgram.objects.all().values_list("name", "bonus_percentage")
+    return LoyaltyProgram.objects.all().values_list(
+        "name",
+        "bonus_percentage"
+    )
 
 
 def not_active_customers() -> QuerySet:
-    return LoyaltyProgramParticipant.objects.filter(last_activity__gt="2021-01-01", last_activity__lt="2022-01-01").values("customer__first_name")
+    return LoyaltyProgramParticipant.objects.filter(
+        last_activity__gt="2021-01-01",
+        last_activity__lt="2022-01-01"
+    ).values("customer__first_name")
 
 
 def most_active_customers() -> QuerySet:
-    return LoyaltyProgramParticipant.objects.filter().values_list("customer__first_name", "customer__last_name", "sum_of_spent_money").order_by("-sum_of_spent_money")[:5]
+    return LoyaltyProgramParticipant.objects.filter().values_list(
+        "customer__first_name",
+        "customer__last_name",
+        "sum_of_spent_money"
+    ).order_by("-sum_of_spent_money")[:5]
 
 
 def clients_with_i_and_o() -> QuerySet:
-    return Customer.objects.filter(Q(first_name__startswith="I") | Q(last_name__contains="o"))
+    return Customer.objects.filter(
+        Q(first_name__startswith="I") | Q(last_name__contains="o"))
 
 
 def bonuses_less_then_spent_money() -> QuerySet:
-    return LoyaltyProgramParticipant.objects.filter(active_bonuses__lt=F("sum_of_spent_money")).values("customer__phone_number")
+    return LoyaltyProgramParticipant.objects.filter(
+        active_bonuses__lt=F("sum_of_spent_money")
+    ).values("customer__phone_number")
