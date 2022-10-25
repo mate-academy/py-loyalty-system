3d182216bbd7e250c90fad57e22733e9756ed42c
Dmytro Skrypka <2346677tyu@gmail.com>
2022-10-25 11:42:13 +0300

    Solution i think the range is correct

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def all_loyalty_program_names() -> QuerySet:
 
 def not_active_customers() -> QuerySet:
     return LoyaltyProgramParticipant.objects.filter(
-        last_activity__year=2021
+        last_activity__range=("2021-01-01", "2021-12-31")
     ).values("customer__first_name")
 
 
