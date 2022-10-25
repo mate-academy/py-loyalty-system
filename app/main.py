45c1ba291d5d1fad9851eb7028994b3dd5d2dc97
Dima Ovcharenko <kukuku381@gmail.com>
2022-10-25 12:52:25 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,23 +1,40 @@
 import init_django_orm  # noqa: F401
 
-from django.db.models import QuerySet
+from django.db.models import QuerySet, Q, F
+
+from db.models import LoyaltyProgram, LoyaltyProgramParticipant
 
 
 def all_loyalty_program_names() -> QuerySet:
-    pass
+    queryset = LoyaltyProgram.objects.values_list("name", "bonus_percentage")
+    return queryset
 
 
 def not_active_customers() -> QuerySet:
-    pass
+    queryset = LoyaltyProgramParticipant.objects.filter(
+        last_activity__year="2021"
+    ).values("customer__first_name")
+    return queryset
 
 
 def most_active_customers() -> QuerySet:
-    pass
+    queryset = LoyaltyProgramParticipant.objects.values_list(
+        "customer__first_name",
+        "customer__last_name",
+        "sum_of_spent_money"
+    ).order_by("-sum_of_spent_money")[:5]
+    return queryset
 
 
 def clients_with_i_and_o() -> QuerySet:
-    pass
+    queryset = LoyaltyProgramParticipant.objects.filter(
+        Q(customer__first_name__startswith="I")
+        | Q(customer__last_name__contains="o"))
+    return queryset
 
 
 def bonuses_less_then_spent_money() -> QuerySet:
-    pass
+    queryset = LoyaltyProgramParticipant.objects.filter(
+        active_bonuses__lt=F("sum_of_spent_money")
+    ).values("customer__phone_number")
+    return queryset
