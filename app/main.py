9f50cc5c978266b8a1ab9dbb8aa517bf204cc61a
Alexander <alexzubkoff123@gmail.com>
2022-10-26 16:30:39 +0300

    Refactored code, added __gte looksup for condition

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def all_loyalty_program_names() -> QuerySet:
 
 def not_active_customers() -> QuerySet:
     return LoyaltyProgramParticipant.objects.filter(
-        last_activity__gt="2021-01-01",
+        last_activity__gte="2021-01-01",
         last_activity__lt="2022-01-01"
     ).values("customer__first_name")
 
