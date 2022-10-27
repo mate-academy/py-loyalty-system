bc0baf527c4b38844fda0963d455a08d3d75168f
Pavlo Kapitanets <aixpavlik"gmail.com>
2022-10-27 18:23:55 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -7,21 +7,25 @@ from db.models import LoyaltyProgram, LoyaltyProgramParticipant
 
 def all_loyalty_program_names() -> QuerySet:
     return LoyaltyProgram.objects.values_list(
-        "name", "bonus_percentage")
+        "name",
+        "bonus_percentage"
+    )
 
 
 def not_active_customers() -> QuerySet:
     return LoyaltyProgramParticipant.objects.filter(
-        last_activity__gt="2021-01-01",
-        last_activity__lt="2022-01-01").values("customer__first_name")
+        last_activity__year="2021"
+    ).values("customer__first_name")
 
 
 def most_active_customers() -> QuerySet:
-    return LoyaltyProgramParticipant.objects.order_by(
-        "-sum_of_spent_money")[:5].values_list(
+    return LoyaltyProgramParticipant.objects.values_list(
         "customer__first_name",
         "customer__last_name",
-        "sum_of_spent_money")
+        "sum_of_spent_money"
+    ).order_by(
+        "-sum_of_spent_money"
+    )[:5]
 
 
 def clients_with_i_and_o() -> QuerySet:
