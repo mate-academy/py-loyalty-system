258bd7effb53e85b22da1fc9a6271d0a2d46706a
Bohdan2001007 <bogdan1991007@gmail.com>
2022-11-14 22:03:08 +0100

    Solution

--- db/models.py
+++ db/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Customer(models.Model):
+    objets = None
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     phone_number = models.CharField(max_length=20)
