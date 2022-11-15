fc43fd20d8b39c89e7d9439576174a54e905ba2c
Paul Starodub <amorallex@gmail.com>
2022-11-15 22:08:13 +0400

    Without all

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from db.models import Customer, LoyaltyProgram, LoyaltyProgramParticipant
 
 
 def all_loyalty_program_names() -> QuerySet:
-    return LoyaltyProgram.objects.all().values_list(
+    return LoyaltyProgram.objects.values_list(
         "name",
         "bonus_percentage"
     )
@@ -20,13 +20,13 @@ def not_active_customers() -> QuerySet:
 
 
 def most_active_customers() -> QuerySet:
-    return LoyaltyProgramParticipant.objects.all().order_by(
+    return LoyaltyProgramParticipant.objects.order_by(
         "-sum_of_spent_money"
-    )[:5].values_list(
+    ).values_list(
         "customer__first_name",
         "customer__last_name",
         "sum_of_spent_money"
-    )
+    )[:5]
 
 
 def clients_with_i_and_o() -> QuerySet:
