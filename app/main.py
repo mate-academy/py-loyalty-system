23378a186b20def18a12a410f48ff20a5dde2f35
Soroka Igor <copokaigor@gmail.com>
2022-11-23 13:54:11 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,21 +1,20 @@
 from django.db.models import QuerySet, Q, F
 
 import init_django_orm  # noqa: F401
-from db.models import LoyaltyProgram, \
-    LoyaltyProgramParticipant, \
-    Customer
+from db.models import LoyaltyProgram, LoyaltyProgramParticipant, Customer
 
 
 def all_loyalty_program_names() -> QuerySet:
-    return LoyaltyProgram. \
-        objects.values_list("name", "bonus_percentage")
+    return LoyaltyProgram.objects.values_list(
+        "name", "bonus_percentage"
+    )
 
 
 def not_active_customers() -> QuerySet:
-    return LoyaltyProgramParticipant.objects. \
-        filter(last_activity__gte="2021-01-01",
-               last_activity__lt="2022-01-01"). \
-        values("customer__first_name")
+    return LoyaltyProgramParticipant.objects.filter(
+        last_activity__year=2021).values(
+        "customer__first_name"
+    )
 
 
 def most_active_customers() -> QuerySet:
@@ -34,7 +33,7 @@ def clients_with_i_and_o() -> QuerySet:
 
 
 def bonuses_less_then_spent_money() -> QuerySet:
-    return LoyaltyProgramParticipant. \
-        objects. \
-        filter(active_bonuses__lt=F("sum_of_spent_money")). \
-        values("customer__phone_number")
+    return LoyaltyProgramParticipant.objects.filter(
+        active_bonuses__lt=F(
+            "sum_of_spent_money")).values("customer__phone_number"
+                                          )
