31713186ef40fe3a7ccf78d208507c79e975cf82
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2022-12-08 15:16:52 +0200

    Solution

--- db/queries.py
+++ db/queries.py
@@ -1,23 +1,40 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
+from django.db.models import F, Q
+from db.models import LoyaltyProgram, Customer, LoyaltyProgramParticipant
 
 
 def all_loyalty_program_names() -> QuerySet:
-    pass
+    return LoyaltyProgram.objects.all().values_list("name", "bonus_percentage")
 
 
 def not_active_customers() -> QuerySet:
-    pass
+    customers = Customer.objects.filter(
+        Q(loyaltyprogramparticipant__last_activity__gt="2021-01-01")
+        & Q(loyaltyprogramparticipant__last_activity__lt="2022-01-01")
+    )
+    return customers.annotate(
+        customer__first_name=F("first_name")
+    ).values("customer__first_name")
 
 
 def most_active_customers() -> QuerySet:
-    pass
+    return Customer.objects.all().order_by(
+        "-loyaltyprogramparticipant__sum_of_spent_money"
+    )[:5].values_list("first_name",
+                      "last_name",
+                      "loyaltyprogramparticipant__sum_of_spent_money")
 
 
 def clients_with_i_and_o() -> QuerySet:
-    pass
+    return Customer.objects.filter(
+        Q(first_name__startswith="I")
+        | Q(last_name__contains="o")
+    )
 
 
 def bonuses_less_then_spent_money() -> QuerySet:
-    pass
+    return LoyaltyProgramParticipant.objects.filter(
+        sum_of_spent_money__gt=F("active_bonuses")
+    ).values("customer__phone_number")
