8d324e103d8dd68bf96b72cd9a2a9fde9603ad30
Mykyta Moshchenko <mykytamoshchenko@outlook.com>
2022-12-24 11:37:05 +0100

    Solution updated with better date-filter for "not_active_customer" function

--- db/queries.py
+++ db/queries.py
@@ -10,8 +10,7 @@ def all_loyalty_program_names() -> QuerySet:
 
 def not_active_customers() -> QuerySet:
     return LoyaltyProgramParticipant.objects.filter(
-        last_activity__gt="2021-01-01",
-        last_activity__lt="2022-01-01"
+        last_activity__year=2021
     ).values("customer__first_name")
 
 
