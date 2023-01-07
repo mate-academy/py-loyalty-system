d9343d7c2e29fd244f326698578d06237a93128f
Andrew.H <john.riderr9@gmail.com>
2023-01-07 10:54:47 +0200

    fixed code style with black

--- db/queries.py
+++ db/queries.py
@@ -13,26 +13,25 @@ def not_active_customers() -> QuerySet:
     start_period = datetime.date(2021, month=1, day=1)
     end_period = datetime.date(2022, month=1, day=1)
     return LoyaltyProgramParticipant.objects.filter(
-        last_activity__gt=start_period,
-        last_activity__lt=end_period).values(
-        "customer__first_name")
+        last_activity__gt=start_period, last_activity__lt=end_period
+    ).values("customer__first_name")
 
 
 def most_active_customers() -> QuerySet:
     return LoyaltyProgramParticipant.objects.order_by(
-        "-sum_of_spent_money").values_list(
-        "customer__first_name",
-        "customer__last_name",
-        "sum_of_spent_money"
+        "-sum_of_spent_money"
+    ).values_list(
+        "customer__first_name", "customer__last_name", "sum_of_spent_money"
     )[:5]
 
 
 def clients_with_i_and_o() -> QuerySet:
     return Customer.objects.filter(
-        Q(first_name__startswith="I") | Q(last_name__contains="o"))
+        Q(first_name__startswith="I") | Q(last_name__contains="o")
+    )
 
 
 def bonuses_less_then_spent_money() -> QuerySet:
     return LoyaltyProgramParticipant.objects.filter(
-        active_bonuses__lt=F("sum_of_spent_money")).values(
-        "customer__phone_number")
+        active_bonuses__lt=F("sum_of_spent_money")
+    ).values("customer__phone_number")
