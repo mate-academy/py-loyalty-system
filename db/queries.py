9d1e9a1363fd914a37f4488564595a9a2133e2b2
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2023-01-27 12:37:09 +0100

    Solution

--- db/queries.py
+++ db/queries.py
@@ -21,7 +21,7 @@ def not_active_customers() -> QuerySet:
 def most_active_customers() -> QuerySet:
     return LoyaltyProgramParticipant.objects.order_by(
         "-sum_of_spent_money"
-    ).filter().values_list(
+    ).values_list(
         "customer__first_name",
         "customer__last_name",
         "sum_of_spent_money"
