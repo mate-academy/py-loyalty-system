c5de3224c3686c0605e35049b08a3b702eda69c2
Piter Abramov <sebshe3@gmail.com>
2023-02-24 14:59:57 +0300

    Solution

--- db/queries.py
+++ db/queries.py
@@ -34,8 +34,7 @@ def most_active_customers() -> QuerySet:
 
 def clients_with_i_and_o() -> QuerySet:
     return Customer.objects.filter(
-        Q(first_name__startswith="I")
-        | Q(last_name__contains="o")
+        Q(first_name__startswith="I") | Q(last_name__contains="o")
     )
 
 
